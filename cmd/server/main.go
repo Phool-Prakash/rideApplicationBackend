f5297f8a72717730142da1716fe45ec30dde44ca
Phool Prakash <phool.prakash@newtabtechnologies.com>
2025-06-29 16:50:46 +0530

    Str

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,18 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"rideApplicationBackend/config"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	_ = godotenv.Load()
+	db := config.ConnectDB()
+	defer db.Disconnect(nil)
+
+	// router :=
+	log.Println("Server Started at :8080")
+	log.Fatal(http.ListenAndServe(":8080",router))
+}
